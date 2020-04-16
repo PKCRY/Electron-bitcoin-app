f136be64c66230e9f23fc0d0963a94192bca964a
Nick Card <nickcard2000@gmail.com>
2020-04-16 00:21:57 -0400

    ignored files

---
+++ main.js
@@ -0,0 +1,214 @@
+// Modules to control application life and create native browser window
+const {app, BrowserWindow, Menu, App} = require('electron')
+const path = require('path')
+const {shell} = require('electron').shell
+
+require('electron-reload')(__dirname)
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {preload: path.join(__dirname, 'preload.js')}
+  });
+  // and load the index.html of the app.
+  win.loadFile('source/index.html')
+  // Open the DevTools.
+  //Win.webContents.openDevTools()
+  // Emitted when the window is closed.
+  win.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on("ready", createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+function addWindow () {
+    // Create the browser window.
+    const addWindow = new BrowserWindow({
+      width: 400,
+      height: 300,
+    })
+    addWindow.loadFile('source/addWindow.html')
+}
+
+
+
+
+
+const template = [
+    {
+        label: 'BitCoin',
+        submenu: [
+            {
+                label: 'Open New Window',
+                click(){
+                    addWindow();
+                }
+            },
+            {
+                label: "Visit CoinMarketCap",
+                click() {
+                shell.openExternal("http://coinmarketcap.com/");
+                }
+            }
+        ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          role: 'undo'
+        },
+        {
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'cut'
+        },
+        {
+          role: 'copy'
+        },
+        {
+          role: 'paste'
+        },
+        {
+          role: 'pasteandmatchstyle'
+        },
+        {
+          role: 'delete'
+        },
+        {
+          role: 'selectall'
+        }
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.reload()
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click (item, focusedWindow) {
+            if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'resetzoom'
+        },
+        {
+          role: 'zoomin'
+        },
+        {
+          role: 'zoomout'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'togglefullscreen'
+        }
+      ]
+    },
+    {
+      role: 'window',
+      submenu: [
+        {
+          role: 'minimize'
+        },
+        {
+          role: 'close'
+        }
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('http://electron.atom.io') }
+        }
+      ]
+    }
+  ]
+  
+  if (process.platform === 'darwin') {
+    const name = app.getName()
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'hide'
+        },
+        {
+          role: 'hideothers'
+        },
+        {
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          role: 'quit'
+        }
+      ]
+    })
+  }
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
---
+++ source/index.js
@@ -0,0 +1,14 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.remote.BrowserWindow
+
+const notifyBtn = document.getElementById('notifyBtn')
+console.log(notifyBtn)
+notifyBtn.addEventListener('click', function (event) {
+    console.log(__dirname)
+    const modalPath = path.join('file://', __dirname, 'addWindow.html')
+    let win = new BrowserWindow({ width: 400, height: 200})
+    win.on('close', function () { win = null })
+    win.loadURL(modalPath)
+    win.show()
+})
